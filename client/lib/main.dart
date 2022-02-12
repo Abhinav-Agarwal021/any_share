1dd92f364c6aecfe0c130129af2a95e5cb6d5e36
Abhinav Agarwal <agarwalabhinav309@gmail.com>
2022-02-12 19:31:53 +0530

    user is permanently logged in

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -8,11 +8,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 
-var userData;
+// void main() async {
+//   final SharedPreferences sharedPreferences =
+//       await SharedPreferences.getInstance();
 
-void main() {
-  runApp(const MyApp());
-}
+//   var obtainData = sharedPreferences.getString("jwt");
+//   debugPrint(obtainData);
+//   runApp(obtainData == null ? const SignupRoute() : const Dashboard());
+// }
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -23,45 +26,11 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  void initState() {
-    getAuthentication().whenComplete(() async {
-      Navigator.pop(context);
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) =>
-                  userData == null ? const SignupRoute() : const Dashboard()));
-    });
-    super.initState();
-  }
-
-  Future getAuthentication() async {
-    final SharedPreferences sharedPreferences =
-        await SharedPreferences.getInstance();
-
-    var obtainData = sharedPreferences.getString("jwt");
-
-    String uri = "http://10.0.2.2:8000/api/user/";
-    var data = {"token": obtainData};
-    var response = await post(Uri.parse(uri),
-        headers: {"Content-Type": "application/json"},
-        body: jsonEncode(data),
-        encoding: Encoding.getByName("utf-8"));
-    String resb = response.body;
-    debugPrint(resb);
-
-    setState(() {
-      userData = response.body;
-    });
-
-    debugPrint(userData);
-  }
-
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: "/",
       routes: {
-        "/": (context) => const Messages(),
+        "/": (context) => const SignupRoute(),
         MyRoutes.dashboard: (context) => const Dashboard(),
         MyRoutes.signupRoute: (context) => const SignupRoute(),
         MyRoutes.chat: (context) => const Chats(),
@@ -69,4 +38,29 @@ class _MyAppState extends State<MyApp> {
       },
     );
   }
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final SharedPreferences sharedPreferences =
+      await SharedPreferences.getInstance();
+
+  var obtainData = sharedPreferences.getString("jwt");
+
+  String uri = "http://10.0.2.2:8000/api/user/";
+  var data = {"token": obtainData};
+  var response = await post(Uri.parse(uri),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(data),
+      encoding: Encoding.getByName("utf-8"));
+  var resb = response.body;
+  debugPrint(resb);
+  var userData;
+
+  if (response.statusCode == 200) {
+    userData = response.body;
+  }
+  runApp(MaterialApp(
+      home: userData == null ? const SignupRoute() : const Dashboard()));
 }
